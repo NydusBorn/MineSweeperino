6157edb9ec07c629f23bf2523ff9453fd957d255
nydus <nydusborn@outlook.com>
2023-07-15 19:07:18 +0300

    Correction to class names

--- MineSweeperAuto/GamePage.xaml.cs
+++ MineSweeperAuto/GamePage.xaml.cs
@@ -21,9 +21,9 @@ namespace MineSweeperAuto
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class GamePage : Page
     {
-        public BlankPage1()
+        public GamePage()
         {
             this.InitializeComponent();
         }
